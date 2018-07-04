4972bd60bf1dfc0a09b332f3cf90d91f92b78432
Vyacheslav Rusakov <vyarus@gmail.com>
2018-07-04 11:26:28 +0700

    gradle 4.8 pom dsl compatibility check

--- src/test/groovy/ru/vyarus/gradle/plugin/pom/PomMergeKitTest.groovy
+++ src/test/groovy/ru/vyarus/gradle/plugin/pom/PomMergeKitTest.groovy
@@ -51,6 +51,59 @@ class PomMergeKitTest extends AbstractKitTest {
         pom.name.text() == 'custom'
     }
 
+    def "Check pom dsl merge (4.8)"() {
+        setup:
+        build("""
+            plugins {
+                id 'java'
+                id 'ru.vyarus.pom'
+            }
+
+            group 'ru.vyarus'
+            version 1.0
+            description 'sample description'
+
+            publishing {
+                publications {
+                    maven(MavenPublication) {
+                        from components.java
+                        pom {
+                            name = 'first'
+                            scm {
+                              url = "http://subversion.example.com/svn/project/trunk/"
+                            }
+                        }
+                    }
+                }
+            }
+
+            pom {
+                name 'custom'
+                scm {
+                  url "http://google.com"
+                }
+            }
+
+            model {
+                tasks.generatePomFileForMavenPublication {
+                    destination = file("\$buildDir/generated-pom.xml")
+                }
+            }
+        """)
+
+        when: "run pom task"
+        run('generatePomFileForMavenPublication')
+
+        def pomFile = file("build/generated-pom.xml")
+        def pom = new XmlParser().parse(pomFile)
+        // for debug
+        println pomFile.getText()
+
+        then: "pom node overridden"
+        pom.name.text() == 'custom'
+        pom.scm.url.text() == 'http://google.com'
+    }
+
     def "Check pom tree recognition modifications"() {
         setup:
         build("""
