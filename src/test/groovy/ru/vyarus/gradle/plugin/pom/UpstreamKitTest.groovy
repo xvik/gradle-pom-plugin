fc1dc419f990319c9d9be566682c564309672d5c
Vyacheslav Rusakov <vyarus@gmail.com>
2023-02-17 00:11:23 +0700

    update upstream test

--- src/test/groovy/ru/vyarus/gradle/plugin/pom/UpstreamKitTest.groovy
+++ src/test/groovy/ru/vyarus/gradle/plugin/pom/UpstreamKitTest.groovy
@@ -6,7 +6,7 @@ package ru.vyarus.gradle.plugin.pom
  */
 class UpstreamKitTest extends AbstractKitTest {
 
-    String GRADLE_VERSION = '7.5'
+    String GRADLE_VERSION = '8.0'
 
     def "Check pom modifications"() {
         setup:
